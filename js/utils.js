f8dcef7dcc40f7f765109184f9fe46038f3a00e8
tratmarshal <trtcourtmarshal@gmail.com>
2026-07-08 14:32:50 +0700

    edit folder

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+function escapeHtml(str) {
+  if (!str) return '';
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
