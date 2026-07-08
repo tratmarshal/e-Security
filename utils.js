9380b765d8a340a7734e96301eff1684bf31492b
tratmarshal <trtcourtmarshal@gmail.com>
2026-07-08 14:35:42 +0700

    edit

--- utils.js
+++
@@ -1,9 +0,0 @@
-function escapeHtml(str) {
-  if (!str) return '';
-  return String(str)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#039;');
-}
