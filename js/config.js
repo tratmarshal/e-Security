f9cb5be0ff6081468e5867551265018249f1670a
tratmarshal <trtcourtmarshal@gmail.com>
2026-07-08 14:41:39 +0700

    edit gas url , liffid

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfy.../exec', // เปลี่ยนเป็น URL Web App
-  LIFF_ID: '2000000000-xxxxxxxx', // เปลี่ยนเป็น LIFF ID
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyrigshyMUC6u3cJeWBseJW1GAYGfzeyb5MjEgj995xX4BZXgkX2UghQUrkGMXvVkBh/exec', // เปลี่ยนเป็น URL Web App
+  LIFF_ID: '2010616678-GGszdIBe', // เปลี่ยนเป็น LIFF ID
   TIMEOUT: 15000,
   RETRY_DELAYS: [500, 1000, 2000],
   DUTY_POINTS: ['ประตูหน้า', 'ประตูหลัง', 'อาคาร A', 'อาคาร B']
