3cd95146d7c41bbff9dff7f699b35ea97218031f
tratmarshal <trtcourtmarshal@gmail.com>
2026-07-10 11:35:14 +0700

    add lifid swap

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var CONFIG = {
   GAS_URL: 'https://script.google.com/macros/s/AKfycbyrigshyMUC6u3cJeWBseJW1GAYGfzeyb5MjEgj995xX4BZXgkX2UghQUrkGMXvVkBh/exec',
   LIFF_ID_DUTY: '2010616678-GGszdIBe',
-  LIFF_ID_SWAP: '2010616678-XXXXXXX', // เปลี่ยนเป็น LIFF ID สำหรับระบบ Swap
+  LIFF_ID_SWAP: '2010616678-xec2qSeT', // เปลี่ยนเป็น LIFF ID สำหรับระบบ Swap
   TIMEOUT: 15000,
   RETRY_DELAYS: [500, 1000, 2000]
 };
