c01b07eeccb56a1390b318c187d451834bee08fd
tratmarshal <trtcourtmarshal@gmail.com>
2026-07-10 13:18:42 +0700

    deploy

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbyrigshyMUC6u3cJeWBseJW1GAYGfzeyb5MjEgj995xX4BZXgkX2UghQUrkGMXvVkBh/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxDUwCRJ9JMFsyqDs4zHECqjp9RNNU4r9bSQ5C_-WMQlQVZkD3BCK0i1pLK3MC81rM/exec',
   LIFF_ID_DUTY: '2010616678-GGszdIBe',
   LIFF_ID_SWAP: '2010616678-xec2qSeT', // เปลี่ยนเป็น LIFF ID สำหรับระบบ Swap
   TIMEOUT: 15000,
